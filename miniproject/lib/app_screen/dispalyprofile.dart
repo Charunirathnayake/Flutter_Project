177e1016112853d23c38a00916623e2829a9e165
Charunirathnayake <charunirathnayake1995@gmail.com>
2019-11-30 16:41:45 +0530

    Create dispalyprofile page

---
+++ miniproject/lib/app_screen/dispalyprofile.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+class Displayprofile extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Tour Guide',
+          style: TextStyle(fontSize: 20.0),
+        ),
+        backgroundColor: Color(0xffBA680B),
+      ),);
+  }
+
+}
+
+class Display extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    
+    return Display_State();
+  }
+
+}
+
+class Display_State extends State<Display>{
+  @override
+  Widget build(BuildContext context) {
+  
+    return ;
+  }
+
+}
