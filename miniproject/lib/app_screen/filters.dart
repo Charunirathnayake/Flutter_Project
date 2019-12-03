a738baa7d5cbbcfaa39cfd1decf1e18eb8e79dc9
Charunirathnayake <charunirathnayake1995@gmail.com>
2019-12-03 22:00:46 +0530

    updated

--- miniproject/lib/app_screen/filters.dart
+++ miniproject/lib/app_screen/filters.dart
@@ -1,58 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 import 'package:loader_search_bar/loader_search_bar.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-
-class FilterList extends StatelessWidget{
+class FilterList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
         body: FilterView(),
-      appBar:AppBar(
-        backgroundColor: Color(0xffBA680B),
-        title: Text('Search for your guide'),
-        actions: <Widget>[
-          IconButton(icon:Icon(Icons.search),onPressed: (){
-              showSearch(context: context, delegate:Datasearchbox() );
-          },
-          
-          ),
-         /*IconButton(icon: Icon(Icons.filter_list),onPressed: (){
+        appBar: AppBar(
+          backgroundColor: Color(0xffBA680B),
+          title: Text('Search for your guide'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                showSearch(context: context, delegate: Datasearchbox());
+              },
+            ),
+            /*IconButton(icon: Icon(Icons.filter_list),onPressed: (){
 
           },)*/
-        ],
-      )
-    );
+          ],
+        ));
   }
-
 }
-class Datasearchbox extends SearchDelegate<String>{
-  final distric=['Rathhnapura','Kegalle','Galle','Mathara'];
-  final recent=['kegalle','Galle'];
+
+class Datasearchbox extends SearchDelegate<String> {
+  final distric = ['Rathhnapura', 'Kegalle', 'Galle', 'Mathara'];
+  final recent = ['kegalle', 'Galle'];
 
   @override
   List<Widget> buildActions(BuildContext context) {
-    
-    return[
-    IconButton(icon: Icon(Icons.clear),onPressed: (){
-      query="";
-      
-    },)
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = "";
+        },
+      )
     ];
-
   }
 
   @override
   Widget buildLeading(BuildContext context) {
     return IconButton(
-      icon:AnimatedIcon(icon:AnimatedIcons.menu_arrow,
-      progress:transitionAnimation,
-      
-    ),onPressed: (){
-      close(context,null);
-    },);
-      }
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
+      ),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
 
   @override
   Widget buildResults(BuildContext context) {
@@ -63,41 +64,239 @@ class Datasearchbox extends SearchDelegate<String>{
   @override
   Widget buildSuggestions(BuildContext context) {
     //TODO:REMOVE UNNESSARY CODES
-    final suggesionsList=query.isEmpty?recent:distric;
-    return ListView.builder(itemBuilder: (context,index){
+    final suggesionsList = query.isEmpty ? recent : distric;
+    return ListView.builder(
+      itemBuilder: (context, index) {
         ListTile(
           leading: Icon(Icons.location_city),
           title: Text(suggesionsList[index]),
         );
-        
-    },itemCount: suggesionsList.length,);
+      },
+      itemCount: suggesionsList.length,
+    );
   }
-
 }
 
-class FilterView extends StatefulWidget{
+class FilterView extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return FilterView_State();
   }
-
 }
-class FilterView_State extends State<FilterView>{
+
+class FilterView_State extends State<FilterView> {
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return ListView(
       children: <Widget>[
-        Text('Explore Your Guide'),
+        Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0),
+              child: Text(
+                'Explore Your Guide',
+                style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+              ),
+            ),
 
-        SizedBox(height:20.0),
+            SizedBox(height: 30.0),
 
-        Text('Guide Passion'),
+            Text(
+              'Guide Passion',
+              style: TextStyle(fontSize: 25.0,color: Color(0xffBA680B),fontWeight: FontWeight.bold),
+            ),
 
-        SizedBox(height:10.0),
+            Divider(
+              height: 8.0,
+              thickness: 2.0,
+              indent: 10.0,
+              endIndent: 10.0,
+            ),
 
-        //
+            SizedBox(height: 10.0),
+
+            //heritage guide
+            Padding(
+              padding: EdgeInsets.only(left: 20.0, right: 20.0),
+              child: RaisedButton(
+                  color: Color(0xff1883F),
+                  hoverColor: Color(0xffF5CA99),
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(40.0),
+                    side: BorderSide(color: Color(0xff1883F)),
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(FontAwesomeIcons.dungeon),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text(
+                        'Heritage Guide',
+                        style: TextStyle(color: Colors.white, fontSize: 20.0),
+                      ),
+                    ],
+                  )),
+            ),
+
+            SizedBox(
+              height: 10.0,
+            ),
+
+            //Cultural guide
+            Padding(
+              padding: EdgeInsets.only(left: 20.0, right: 20.0),
+              child: RaisedButton(
+                  color: Color(0xffBA680B),
+                  hoverColor: Color(0xffF5CA99),
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(40.0),
+                    side: BorderSide(color: Color(0xffBA680B)),
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(FontAwesomeIcons.atom),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text(
+                        'Cultural Guide',
+                        style: TextStyle(color: Colors.white, fontSize: 20.0),
+                      ),
+                    ],
+                  )),
+            ),
+
+            SizedBox(height: 10.0),
+
+            //Adventure guide
+            Padding(
+              padding: EdgeInsets.only(left: 20.0, right: 20.0),
+              child: RaisedButton(
+                  color: Color(0xffBA680B),
+                  hoverColor: Color(0xffF5CA99),
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(40.0),
+                    side: BorderSide(color: Color(0xffBA680B)),
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(FontAwesomeIcons.hiking),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text(
+                        'Adventure Guide',
+                        style: TextStyle(color: Colors.white, fontSize: 20.0),
+                      ),
+                    ],
+                  )),
+            ),
+
+            SizedBox(
+              height: 10.0,
+            ),
+
+            //Forest guide
+            Padding(
+              padding: EdgeInsets.only(left: 20.0, right: 20.0),
+              child: RaisedButton(
+                  color: Color(0xffBA680B),
+                  hoverColor: Color(0xffF5CA99),
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(40.0),
+                    side: BorderSide(color: Color(0xffBA680B)),
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(FontAwesomeIcons.tree),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text(
+                        'Forest Guide',
+                        style: TextStyle(color: Colors.white, fontSize: 20.0),
+                      ),
+                    ],
+                  )),
+            ),
+
+            SizedBox(
+              height: 10.0,
+            ),
+
+            //Business guide
+            Padding(
+              padding: EdgeInsets.only(left: 20.0, right: 20.0),
+              child: RaisedButton(
+                  color: Color(0xffBA680B),
+                  hoverColor: Color(0xffF5CA99),
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(40.0),
+                    side: BorderSide(color: Color(0xffBA680B)),
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(FontAwesomeIcons.building),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text(
+                        'Business Guide',
+                        style: TextStyle(color: Colors.white, fontSize: 20.0),
+                      ),
+                    ],
+                  )),
+            ),
+
+            SizedBox(height: 10.0),
+
+            //Nature guide
+            Padding(
+              padding: EdgeInsets.only(left: 20.0, right: 20.0),
+              child: RaisedButton(
+                  color: Color(0xffBA680B),
+                  hoverColor: Color(0xffF5CA99),
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(40.0),
+                    side: BorderSide(color: Color(0xffBA680B)),
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(FontAwesomeIcons.binoculars),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text(
+                        'Business Guide',
+                        style: TextStyle(color: Colors.white, fontSize: 20.0),
+                      ),
+                    ],
+                  )),
+            ),
+          ],
+        ),
+
+        SizedBox(height: 10.0,),
+
+        Divider(
+          height: 8.0,
+          thickness: 2.0,
+          indent: 10.0,
+          endIndent: 10.0,
+        ),
+
+        SizedBox(height: 20.0,),
+
+        //Explore button
         Padding(
-          padding: EdgeInsets.only(left: 20.0,right: 20.0),
+          padding: EdgeInsets.only(left: 20.0, right: 20.0),
           child: RaisedButton(
             color: Color(0xffBA680B),
             hoverColor: Color(0xffF5CA99),
@@ -106,14 +305,13 @@ class FilterView_State extends State<FilterView>{
               borderRadius: BorderRadius.circular(40.0),
               side: BorderSide(color: Color(0xffBA680B)),
             ),
-            child: Text('Send new password link',style:TextStyle(color: Colors.white,fontSize: 20.0),)
+            child: Text(
+              'Explore your Guide',
+              style: TextStyle(color: Colors.white, fontSize: 20.0),
+            ),
           ),
         ),
-
       ],
     );
   }
-
 }
-
-
