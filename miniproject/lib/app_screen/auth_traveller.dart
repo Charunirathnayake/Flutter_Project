c04f70cb307905e6677ca8b090eac3e5b23a7187
Charunirathnayake <charunirathnayake1995@gmail.com>
2019-12-27 23:09:14 +0530

    updated

---
+++ miniproject/lib/app_screen/auth_traveller.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class Auth{
+  Future<bool> signInWithEmail(String email, String password) async {
+    try {
+      AuthResult result = await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: email, password: password);
+      FirebaseUser user = result.user;
+      if (user != null) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      return false;
+    }
+  }
+}
